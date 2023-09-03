832ebed033e3d18cad41a6f15d01dd7006dd4cc8
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2023-09-03 21:43:31 +0530

    Basic Api created

---
+++ index.js
@@ -0,0 +1,14 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.get("/",(req,res)=>{
+    res.send("Routing working")
+})
+
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server connected on port:${process.env.PORT}`)
+})
